3c98258c1a8a3a8a1970b66012d85652633282eb
aishraq16 <ishraqafzal16@gmail.com>
2026-08-18 23:47:57 +0800

    set up giphy api stuff

--- src/services/main.js
+++ src/services/main.js
@@ -3,9 +3,9 @@ import 'dotenv/config';
 const WEATHER_BASE_URL = process.env.WEATHER_BASE_URL
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 const location = "dhaka"
-const URL = `${WEATHER_BASE_URL}${location}?key=${WEATHER_API_KEY}`
-console.log(URL)
-fetch(URL)
+const weather_url = `${WEATHER_BASE_URL}${location}?key=${WEATHER_API_KEY}`
+console.log(weather_url)
+fetch(weather_url)
     .then((res) => {
         if (!res){
             throw new Error(`HTTP Error. Status: ${res.status}`)
@@ -17,3 +17,21 @@ fetch(URL)
     })
     .catch((err) => console.log("error: " + err))
 
+const GIF_BASE_URL = process.env.GIF_BASE_URL
+const GIF_API_KEY = process.env.GIF_API_KEY
+const search = "rain"
+const gif_url = `${GIF_BASE_URL}?api_key=${GIF_API_KEY}&q=${search}`
+console.log(gif_url)
+fetch(gif_url)
+    .then(res => {
+        if (!res){
+            throw new Error(`HTTP Error. Status: ${res.status}`)
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data.data[0].url)
+    })
+    .catch(err => {
+        console.log("Error: " + err)
+    })
